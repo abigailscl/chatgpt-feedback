dea250bc4be6a1b605d88b9fb33cca17693d5a3a
Abigail <abigail.cabascango@ioet.com>
2024-06-16 16:53:50 -0500

    build: fastapi setup

---
+++ app/tests/integration/infraestructure/main_test.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+
+from app.infraestructure.api.main import app
+
+
+client = TestClient(app)
+
+
+def test__health_check():
+    expected_response = {
+        "name": "AI Feedback",
+        "version": "0.0.1"
+    }
+
+    response = client.get("/health-check")
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
