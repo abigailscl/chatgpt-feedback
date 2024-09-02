9c8491bfdbb259593eb53dfb5b01bc8ec472bea6
Abigail <abigail.cabascango@ioet.com>
2024-09-01 23:23:16 -0500

    fix: ACCESS_TOKEN_EXPIRE_MINUTES default value

--- app/config/enviroment.py
+++ app/config/enviroment.py
@@ -11,7 +11,7 @@ class Settings:
     URL_DATABASE: str = os.getenv("URL_DATABASE")
     SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
     ALGORITHM: str = os.getenv("JWT_ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 settings = Settings()
