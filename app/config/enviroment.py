046ca155c7a8e78f0b3215f590406d37c191c8ac
Abigail <abigail.cabascango@ioet.com>
2024-09-01 23:32:26 -0500

    test

--- app/config/enviroment.py
+++ app/config/enviroment.py
@@ -12,6 +12,7 @@ class Settings:
     SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
     ALGORITHM: str = os.getenv("JWT_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    print('__mintes__', ACCESS_TOKEN_EXPIRE_MINUTES)
 
 
 settings = Settings()
